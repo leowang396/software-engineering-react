7957bf8cb091caa9fbb52f46b7a2733e6bd93df8
Shengdi Wang <shengdi0313@gmail.com>
2022-11-20 14:33:27 -0500

    Adds tests for both dislikes-service and UI components; modifies URLs used in services to accomodate URL issues when axios is run by jest

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 
 const LOGIN_API = `${BASE_URL}/api/login`;
 const USERS_API = `${BASE_URL}/api/users`;
