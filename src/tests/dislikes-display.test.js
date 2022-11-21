5adaf4d14da274c8c6719a3fe9d5eb259b636f6b
Shengdi Wang <shengdi0313@gmail.com>
2022-11-20 21:52:09 -0500

    Adds tests for my dislikes screen

--- src/tests/dislikes-display.test.js
+++ src/tests/dislikes-display.test.js
@@ -8,7 +8,7 @@ import {HashRouter} from "react-router-dom";
 import {createTuitByUser, deleteTuit, findAllTuits} from "../services/tuits-service";
 import {createUser, deleteUsersByUsername} from "../services/users-service";
 import * as React from "react";
-import {userTogglesTuitDislikes} from "../services/dislikes-service";
+import {findAllTuitsDislikedByUser, userTogglesTuitDislikes} from "../services/dislikes-service";
 import {userTogglesTuitLikes} from "../services/likes-service";
 
 const MOCKED_USER = {username: 'alice', password: 'lv426', email: 'alice@weyland.com'};
@@ -57,11 +57,12 @@ test('neural tuit is rendered correctly after dislikes', async () => {
     expect(linkElement).toContainHTML(`<i class="fa-solid fa-thumbs-down" style="color: red;" />`);
 
     // Cleans up.
+    await userTogglesTuitDislikes(testUser._id, createdTuit._id);
     await deleteTuit(createdTuit._id);
     await deleteUsersByUsername(MOCKED_USER.username)
 })
 
-test('disliked tuits is rendered correctly after getting another dislike', async () => {
+test('disliked tuit is rendered correctly after getting another dislike', async () => {
     // Creates a propert Tuit object by POSTing a sample user and tuit to the server.
     const testUser = await createUser(MOCKED_USER);
     const createdTuit = await createTuitByUser(testUser._id, MOCKED_TUITS[2].tuit);
@@ -82,7 +83,7 @@ test('disliked tuits is rendered correctly after getting another dislike', async
     await deleteUsersByUsername(MOCKED_USER.username)
 })
 
-test('liked tuits is rendered correctly after getting disliked', async () => {
+test('liked tuit is rendered correctly after getting disliked', async () => {
     // Creates a propert Tuit object by POSTing a sample user and tuit to the server.
     const testUser = await createUser(MOCKED_USER);
     const createdTuit = await createTuitByUser(testUser._id, MOCKED_TUITS[2].tuit);
@@ -99,6 +100,38 @@ test('liked tuits is rendered correctly after getting disliked', async () => {
     expect(linkElement).toContainHTML(`<i class="fa-solid fa-thumbs-down" style="color: red;" />`);
 
     // Cleans up.
+    await userTogglesTuitDislikes(testUser._id, createdTuit._id);
+    await deleteTuit(createdTuit._id);
+    await deleteUsersByUsername(MOCKED_USER.username)
+})
+
+test('renders static content correctly', async () => {
+    const tuits = MOCKED_TUITS;
+
+    render(
+        <HashRouter>
+            <Tuits tuits={tuits}/>
+        </HashRouter>);
+
+    const linkElement = screen.getByText(/charlie's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+})
+
+test('my-dislikes renders RESTful response correctly', async () => {
+    const testUser = await createUser(MOCKED_USER);
+    const createdTuit = await createTuitByUser(testUser._id, MOCKED_TUITS[2].tuit);
+    await userTogglesTuitDislikes(testUser._id, createdTuit._id);
+
+    const tuits = await findAllTuitsDislikedByUser(testUser._id);
+    render(
+        <HashRouter>
+            <Tuits tuits={tuits}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/charlie's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+
+    // Cleans up.
+    await userTogglesTuitDislikes(testUser._id, createdTuit._id);
     await deleteTuit(createdTuit._id);
     await deleteUsersByUsername(MOCKED_USER.username)
 })
